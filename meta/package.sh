ab04e192a165d849799e94fbccf47d851778d41d
U-UNITED\friebe <thekid@thekid.de>
2018-03-30 15:07:59 +0200

    Clean up generated files after creating .deb package

--- meta/package.sh
+++ meta/package.sh
@@ -5,6 +5,7 @@ TARGET=$2
 
 # Replace template
 cat control.in | sed -s 's/@@PROVIDES@@/'$PROVIDES'/g' > $2/control
+cat $2/control
 
 # Create debian package contents
 cd $2
@@ -14,3 +15,7 @@ fakeroot tar cfJ data.tar.xz --files-from /dev/null
 
 # Build Debian package
 ar -q php-cli-meta.deb debian-binary control.tar.gz data.tar.xz
+ls -al $2
+
+# Cleanup
+rm debian-binary control.tar.gz data.tar.xz
