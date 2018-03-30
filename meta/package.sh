b6da586f6eb7baadb53cd93b34a815a7e5f9287c
U-UNITED\friebe <thekid@thekid.de>
2018-03-30 14:52:27 +0200

    Build meta packages to provide PHP

---
+++ meta/package.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+PROVIDES=$1
+TARGET=$2
+
+# Replace template
+cat control.in | sed -s 's/@@PROVIDES@@/'$PROVIDES'/g' > $2/control
+
+# Create debian package contents
+cd $2
+echo '2.0' > debian-binary
+fakeroot tar cfz control.tar.gz control
+fakeroot tar cfJ data.tar.xz --files-from /dev/null
+
+# Build Debian package
+ar -q php-cli-meta.deb debian-binary control.tar.gz data.tar.xz
